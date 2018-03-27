aede3a7ff7653bbf00edfaa6a7e6614f9c8225af
Tripper6656 <rockyroad6656@yahoo.com>
2018-03-27 12:28:50 -0500

    Set up URL to find coordinates of address

--- main.js
+++ main.js
@@ -11,5 +11,10 @@ function getValue(id) {
 function requestLocation(text){
     //parse text and put in https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
     var arr = text.split(" ");
-    alert(arr[1]);
+    var address = arr[0];
+    for(i = 1; i < arr.length; i++){
+        address = address + '+' + arr[i];
+    }
+    webAddr = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyC53LdjmgLbZZvBXb3W8r1kpdW2UqPJfVE";
+    alert(webAddr);
 }
