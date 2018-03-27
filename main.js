a5157737336c56659d1d0dbe51df3bf680f34d1a
Tripper6656 <rockyroad6656@yahoo.com>
2018-03-27 11:58:18 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var locRequest;
+var apiKey = AIzaSyC53LdjmgLbZZvBXb3W8r1kpdW2UqPJfVE;
+
+function getValue(id) {
+    text = document.getElementById(id).value;
+    document.write(text);
+    requestLocation(text);
+    return false;
+}
+
+function requestLocation(text){
+    //parse text and put in https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+}
